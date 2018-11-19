e13065dc68b28d89c7f8c58b9a23505e47be8ebe
mathieuvu-noreau <mvunorea@mail.uoguelph.ca>
2018-11-19 11:43:08 -0500

    reverted app.hs

--- App.js
+++ App.js
@@ -1,57 +1,3 @@
-
-import React from "react";
-import {
-  Button,
-  View,
-  Text
-} from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation";
-
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress = {
-            () => this.props.navigation.navigate('Details')
-          }
-        />
-      </View>
-    );
-  }
-}
-
-class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Details Screen</Text>
-        <Button
-          title="Go Home"
-          onPress={
-            () => this.props.navigation.navigate('Home')
-          }
-        />
-      </View>
-    );
-  }
-}
-
-const RootStack = createStackNavigator({
-  Home: HomeScreen,
-  Details: DetailsScreen
-}, {
-  initialRouteName: "Home"
-});
-
-
-export default createAppContainer(RootStack);
-
-
-
-/*
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions, Animated, Image, PanResponder } from 'react-native';
 
@@ -217,4 +163,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});*/
+});
