e1c958e05b55859e7c2540d0ffc2b0fab254eb36
mathieuvu-noreau <mvunorea@mail.uoguelph.ca>
2018-11-20 18:05:02 -0500

    data structure create

--- TinderDemo.js
+++ TinderDemo.js
@@ -6,11 +6,54 @@ const SCREEN_WIDTH = Dimensions.get('window').width;
 
 
 const Listings = [
-  {id: "1", uri: require('./assets/house1.jpg')},
-  {id: "2", uri: require('./assets/house2.jpg')},
-  {id: "3", uri: require('./assets/house3.jpg')},
-  {id: "4", uri: require('./assets/house4.jpg')},
-  {id: "5", uri: require('./assets/house5.jpg')}
+  {
+    id: "1",
+    title: "1 Bedroom, 1 Bathroom",
+    location: "Guelph, Ontario",
+    price: "$500/month",
+    ammenties: {
+      parking: true,
+      laundry: true,
+      furnished: false,
+      kitchen: true,
+      petFriendly: false,
+      airCondition: true,
+      roommates: false,
+      smokeFriendly: false,
+    },
+    images: {
+      one: require('./assets/icons/house-pictures/picture1.png'),
+      two: require('./assets/icons/house-pictures/picture2.png'),
+      three: require('./assets/icons/house-pictures/picture3.png'),
+      four: require('./assets/icons/house-pictures/picture4.png'),
+      five: require('./assets/icons/house-pictures/picture5.png'),
+    },
+    description: "A room for rent in a shared house for students, available immediately, 15 minutes walk to U of G, close to Stone Road Mall and Metro grocery. Rent includes heat, hydro, central air, internet and all utilities, laundry is on site with limited parking as well. Plus the house is cleaned once a month by a professional cleaning service.",
+  },
+  { 
+    id: "2",
+    images: {
+      one: require('./assets/house2.jpg')
+    },
+  },
+  { 
+    id: "3", 
+    images: {
+      one: require('./assets/house3.jpg')
+    },
+  },
+  { 
+    id: "4", 
+    images: {
+      one: require('./assets/house4.jpg')
+    },
+  },
+  { 
+    id: "5", 
+    images: {
+      outputRange: require('./assets/house5.jpg')
+    },
+  }
 ]
 export class TinderDemo extends React.Component {
 
@@ -111,14 +154,14 @@ export class TinderDemo extends React.Component {
             key={ item.id }
             style={[this.rotateAndTranslate, { height:SCREEN_HEIGHT-120, width:SCREEN_WIDTH, padding: 10, position: 'absolute' }]}>
               <Animated.View style={{ opacity: this.likeOpacity, transform: [{ rotate: '-30deg'}], position: 'absolute', top: 50, left: 40, zIndex: 1000 }}>
-                <Text style={{ borderWidth: 1, borderColor: 'green', color: 'green', fontSize: 32, fontWeight: '800', padding: 10 }}>LIKE</Text>
+                <Text style={styles.like}>LIKE</Text>
               </Animated.View>
               <Animated.View style={{ opacity: this.dislikeOpacity, transform: [{ rotate: '30deg'}], position: 'absolute', top: 50, right: 40, zIndex: 1000 }}>
-                <Text style={{ borderWidth: 1, borderColor: 'red', color: 'red', fontSize: 32, fontWeight: '800', padding: 10 }}>NOPE</Text>
+                <Text style={styles.dislike}>NOPE</Text>
               </Animated.View>
               <Image
-                style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }}
-                source={ item.uri }
+                style={ styles.homeImage }
+                source={ item.images.one }
               />
           </Animated.View>
         );
@@ -129,8 +172,8 @@ export class TinderDemo extends React.Component {
             key={ item.id }
             style={[ {opacity: this.nextCardOpacity, transform:[{scale:this.nextCardScale}]}, {height:SCREEN_HEIGHT-120, width:SCREEN_WIDTH, padding: 10, position: 'absolute' }]}>
               <Image
-                style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }}
-                source={ item.uri }
+                style={styles.homeImage}
+                source={ item.images.one }
               />
           </Animated.View>
         );
@@ -162,5 +205,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  homeImage: {
+    flex: 1, 
+    height: null, 
+    width: null, 
+    resizeMode: 'cover', 
+    borderRadius: 10,
+  },
+  dislike: {
+    borderWidth: 1, 
+    borderColor: 'red', 
+    color: 'red', 
+    fontSize: 32, 
+    fontWeight: '800', 
+    padding: 10,
+  },
+  like: {
+    borderWidth: 1, 
+    borderColor: 'green', 
+    color: 'green', 
+    fontSize: 32, 
+    fontWeight: '800', 
+    padding: 10
   },
 });
