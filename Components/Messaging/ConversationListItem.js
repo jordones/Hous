f98a5199b839ac1b81ccd0c40fe3c59c87b90d0c
Jordon Smith <jsmith72@mail.uoguelph.ca>
2018-11-24 20:37:27 -0500

    Clickable ConversationListItems
    Wrapped ConversationListItem in TouchableOpacity.

--- Components/Messaging/ConversationListItem.js
+++ Components/Messaging/ConversationListItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TouchableHighlight, View, Image, Text, StyleSheet} from 'react-native';
+import {TouchableOpacity, View, Image, Text, StyleSheet, Alert} from 'react-native';
 import {ChatBubble} from './ChatBubble.js';
 import Styles from './ConversationListStyles.js'
 
@@ -13,29 +13,36 @@ export class ConversationListItem extends React.Component {
 
         }
     }
+
+    _onPressButton() {
+   Alert.alert('You opened a chat!')
+ }
+
     render() {
         return (
+            <TouchableOpacity onPress={this._onPressButton}>
             <View style={Styles.cardRoot}>
-              <View style={Styles.imageContainer}>
-              <ChatBubble source={this.state.source} title=""/>
+                  <View style={Styles.imageContainer}>
+                  <ChatBubble source={this.state.source} title=""/>
+                  </View>
+                  <View style={Styles.rightContainer}>
+                  <Text
+                    style={Styles.text}
+                    numberOfLines={1}
+                    ellipsizeMode={'tail'}
+                  >
+                        {this.state.title}
+                  </Text>
+                  <Text
+                    style={Styles.message}
+                    numberOfLines={1}
+                    ellipsizeMode={'tail'}
+                  >
+                    { this.state.message}
+                  </Text>
+                  </View>
               </View>
-              <View style={Styles.rightContainer}>
-              <Text
-                style={Styles.text}
-                numberOfLines={1}
-                ellipsizeMode={'tail'}
-              >
-                    {this.state.title}
-              </Text>
-              <Text
-                style={Styles.message}
-                numberOfLines={1}
-                ellipsizeMode={'tail'}
-              >
-                { this.state.message}
-              </Text>
-              </View>
-              </View>
+              </TouchableOpacity>
         );
     }
 }
